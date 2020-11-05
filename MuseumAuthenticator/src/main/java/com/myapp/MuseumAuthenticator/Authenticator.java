ca2e3c8b6bce1adc7f91099d98ec16702bbf2ace
byarkan <47678536+byarkan@users.noreply.github.com>
2020-11-05 23:03:41 +0100

    Authenticator module for museum app

---
+++ MuseumAuthenticator/src/main/java/com/myapp/MuseumAuthenticator/Authenticator.java
@@ -0,0 +1,11 @@
+package com.myapp.MuseumAuthenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Authenticator {
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(Authenticator.class, args);
+	}
+}
---
+++ MuseumAuthenticator/src/test/java/com/myapp/MuseumAuthenticator/AuthenticatorTests.java
@@ -0,0 +1,13 @@
+package com.myapp.MuseumAuthenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatorTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
